f546c995982c37820b8225969d66de9ddebb1111
Rex Ye <rxye1991@gmail.com>
2020-09-29 11:48:40 -0400

    installedReactRouter

--- amazonclonefrontend/src/App.js
+++ amazonclonefrontend/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom'
-import './App.css';
+import './App.css'
 
 function App() {
-
+  
   return (
-    <div className="grid-container">
-        <header className='header'>
-            <div className='brand'>
-                <button>#9776</button>
-                <Link to ='/'>Amonja</Link>
-            </div>
-            <div className='header-links'>
-              <a href='cart.html'>Cart</a>
-            </div>
-        </header>
+    <div className='app' >
+
     </div>
   );
 }
