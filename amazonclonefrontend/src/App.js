b7d239aa95caee8e0ef29a89c8ab396274117156
Rex Ye <rxye1991@gmail.com>
2020-10-04 19:37:42 -0400

    createdLoginComponent

--- amazonclonefrontend/src/App.js
+++ amazonclonefrontend/src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+import {Browser as Router, Switch, Route} from 'react-router-dom'
+import Login from './Components/Login'
 import './App.css'
 
+// Components
+import Navbar from './Components//Navbar/Narbar'
 function App() {
-  
   return (
     <div className='app' >
-
+        <Navbar/>
+          <Switch>
+              <Route path='/checkout'>
+                  <h1>Checkout</h1>
+              </Route>
+              <Route path='/login' component={Login}/>
+              <Route path='/'>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+                  <h1>Home Page</h1>
+              </Route>
+          </Switch>
     </div>
   );
 }
---
+++ amazonclonefrontend/src/Components/Navbar/Narbar.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
+import './Narbar.css'
+
+function Narbar() {
+    return (
+        <nav className='header'>
+            <Link>
+                <img
+                    className='header_logo'
+                    src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+                /> 
+            </Link>
+
+        <div className='header_search'>
+            <input type='text' className='header_searchInput'/>
+            <SearchIcon className='header_searchIcon' />
+        </div>
+
+        <div className='header_nav'>
+            
+                <Link to='/login' className='header_link'>
+                    <div className='header_option'>
+                        <span className='header_optionLine1'>Hello</span>
+                        <span className='header_optionLine2'>Sign In</span>
+                    </div>
+                </Link>
+                <Link to='/' className='header_link'>
+                    <div className='header_option'>
+                        <span className='header_optionLine1'>Returns</span>
+                        <span className='header_optionLine2'>Orders</span>
+                    </div>
+                </Link>
+                <Link to='/' className='header_link'>
+                    <div className='header_option'>
+                        <span className='header_optionLine1'>Your</span>
+                        <span className='header_optionLine2'>Prime</span>
+                    </div>
+                </Link>
+
+                <Link to='/checkout' className='header_link'>
+                    <div className='header_optionCheckout'>
+                            <ShoppingBasketIcon />
+                            <span className='header_optionLine2 header_itemsCount' >0</span>
+                    </div>
+                </Link>
+        </div>
+        </nav>
+    )
+}
+
+export default Narbar
