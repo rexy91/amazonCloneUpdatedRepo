c76d0c1234dbc90d1f0c83a05cdc1e7209b4168d
Rex Ye <rxye1991@gmail.com>
2020-10-06 16:00:07 -0400

    workingOnProductCard

--- amazonclonefrontend/src/Components/LandingPage/LandingPage.js
+++ amazonclonefrontend/src/Components/LandingPage/LandingPage.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './LandingPage.css'
-import LandingImage from '../../Components/landing_image.jpg' 
+import LandingImage from '../../Components/landing_image.jpg'
+import Product from '../Product/Product' 
+
 export default function LandingPage() {
     return (
         <div className='landing_page'>
+
                 <img className='landing_image' src={LandingImage}></img>
+
+                <Product 
+                id='1234567'
+                title='off-white Sneaker, blalbasdjgkjb klgjasdlfj kfjsadlkfj sdakljflk '
+                price={499}
+                rating={5}
+                image='https://img.zolaprod.babsta.net/pzY8KTdkSQqtY06iq9jqBkvemzk=/fit-in/850x850/0d0dd09f337547879ff7d6a3a1539094'
+                />
         </div>
+
+
     )
 }
---
+++ amazonclonefrontend/src/Components/Product/Product.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Product.css'
+
+
+const Product = ({title, price, rating, image}) =>  {
+    return (
+        <div className ='product_card'>
+            <div className='product_information'>
+                <p>{title}</p>
+                <p className='product_price'>
+                        <small>$</small>
+                        <strong>{price}</strong>
+                </p>
+                <div className='product_rating'>
+                        {
+                            Array(rating) // Create array with size according to the rating.
+                            .fill() // Fill with empty values
+                            .map(()=> (
+                                    <p>⭐️</p>
+                            ))
+                        }
+                </div>
+            </div>
+            
+                <img src={image}></img>
+                <button>Add to basket</button>
+        </div>
+    )
+}
+
+export default Product
+
+
